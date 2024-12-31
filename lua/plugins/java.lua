797b3653a908c829320826b9ba23029e88602e85
CompileArtisan <praanesh.b.nair@gmail.com>
2024-12-31 15:34:20 +0530

    edited the java.lua file

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -3,6 +3,96 @@ return {
     "mfussenegger/nvim-jdtls",
     ft = { "java" },
     dependencies = { "folke/which-key.nvim" },
+    config = function()
+      -- Get paths without spaces for java executable
+      local function escape_path(path)
+        return vim.fn.substitute(path, " ", "\\ ", "g")
+      end
+      
+      local java_cmd = vim.fn.exepath("java")
+      local mason_path = vim.fn.stdpath("data") .. "/mason"
+      local workspace_path = vim.fn.expand("~/workspace/")
+      local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+      local project_workspace = workspace_path .. project_name
+      
+      -- Create workspace directory if it doesn't exist
+      if vim.fn.isdirectory(project_workspace) == 0 then
+        vim.fn.mkdir(project_workspace, "p")
+      end
+
+      local config = {
+        cmd = {
+          escape_path(java_cmd),
+          '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+          '-Dosgi.bundles.defaultStartLevel=4',
+          '-Declipse.product=org.eclipse.jdt.ls.core.product',
+          '-Dlog.protocol=true',
+          '-Dlog.level=ALL',
+          '-Xmx1g',
+          '--add-modules=ALL-SYSTEM',
+          '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+          '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+          '-jar', escape_path(vim.fn.glob(mason_path .. "/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar")),
+          '-configuration', escape_path(mason_path .. "/packages/jdtls/config_win"),
+          '-data', escape_path(project_workspace)
+        },
+
+        -- Language server settings
+        settings = {
+          java = {
+            signatureHelp = { enabled = true },
+            contentProvider = { preferred = 'fernflower' },
+            completion = {
+              favoriteStaticMembers = {
+                "org.junit.Assert.*",
+                "org.junit.jupiter.api.Assertions.*",
+                "java.util.Objects.requireNonNull",
+                "java.util.Objects.requireNonNullElse",
+              },
+            },
+            sources = {
+              organizeImports = {
+                starThreshold = 9999,
+                staticStarThreshold = 9999,
+              },
+            },
+          },
+        },
+
+        on_attach = function(client, bufnr)
+          -- Enable completion triggered by <c-x><c-o>
+          vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+          
+          local opts = { buffer = bufnr }
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+        end,
+
+        -- Enable codelens
+        init_options = {
+          bundles = {},
+          extendedClientCapabilities = {
+            progressReportProvider = true,
+            classFileContentsSupport = true,
+            generateToStringPromptSupport = true,
+            hashCodeEqualsPromptSupport = true,
+            advancedExtractRefactoringSupport = true,
+            advancedOrganizeImportsSupport = true,
+            generateConstructorsPromptSupport = true,
+            generateDelegateMethodsPromptSupport = true,
+            moveRefactoringSupport = true,
+            overrideMethodsPromptSupport = true,
+            inferSelectionSupport = { "extractMethod", "extractVariable", "extractConstant" }
+          }
+        }
+      }
+
+      -- Start or attach the language server
+      require('jdtls').start_or_attach(config)
+    end,
   },
   {
     "williamboman/mason.nvim",
